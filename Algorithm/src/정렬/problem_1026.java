901f1e2a5858c97a9a914014dd20e69d6a5fef2e
SHPark <suhyang09@naver.com>
2018-01-15 17:44:53 +0900

    보물 1026

---
+++ Algorithm/src/정렬/problem_1026.java
@@ -0,0 +1,38 @@
+package 정렬;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problem_1026 {
+
+	// 보물
+	/*
+	 * 길이가 N인 정수 배열 A와 B가 있다. 다음과 같이 함수 S를 정의하자. S = A[0]*B[0] + ... +
+	 * A[N-1]*B[N-1] S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자.
+	 */
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int sum=0;
+
+		int[] A = new int[N];
+		int[] B = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			B[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		for(int i=0; i<N; i++){
+			sum += A[i]*B[N-1-i];
+		}
+		
+		System.out.println(sum);
+	}
+}
